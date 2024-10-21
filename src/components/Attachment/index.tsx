c6516d3217dd3635bf3b1f4682344192ba9386bc
Alex <alex.wyett@gmail.com>
2024-10-21 06:13:44 +0100

    fix: build

--- src/components/Attachment/index.tsx
+++ src/components/Attachment/index.tsx
@@ -1,9 +1,10 @@
-import { PageFile } from "@/lib/s3";
+import { S3 } from "@cagen/ezsite-components";
+import { S3_BASE_URL } from "@/constants";
 
-export default function Attachment(file: PageFile) {
+export default function Attachment(file: S3.PageFile) {
   return (
     <li>
-      <a href={file.url} target="_blank">
+      <a href={`${S3_BASE_URL}${file.Key}`} target="_blank">
         {file.filename}
         {file.last_modified && <span className="block italic text-xs">Last modified: {file.last_modified.toLocaleDateString()}</span>}
       </a>
